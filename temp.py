9d1c4ff805d7ded8f73b39e703cf875d7ab042c6
adamnark <airdong@gmail.com>
2013-02-16 20:15:41 +0200

    (for testing) added verbosity

--- temp.py
+++ temp.py
@@ -2,6 +2,7 @@ import sql_functions
 imei = '863070011991451'
 data = 'imei:863070011991451,tracker,1301020229,,F,182949.000,A,3205.5019,N,03447.3352,E,21.00,11.0,;' 
 
+print 'testing function sql_functions.write_location_to_log(imei,data)...'
 res = sql_functions.write_location_to_log(imei,data)
-print res
+print 'function returned: ' + res
 sql_functions.check_alerts(res)
