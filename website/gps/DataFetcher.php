a5fb8faa5f45bf2152048485d6d6835ed38d224a
amirsolav <amirsolav@gmail.com>
2013-02-22 18:32:05 +0200

    remove file logging

--- website/gps/DataFetcher.php
+++ website/gps/DataFetcher.php
@@ -17,14 +17,11 @@ class DataFetcher
     
     function get_user_units($user_id)
     {
-        $f = fopen('fetcher log.txt', 'w');
-        
         $this->conn->connect();
-        fwrite($f, "connection established!\r\n");
         $user_units = $this->conn->get_units($user_id);
-        fwrite($f, "got units for user_id=". $user_id ."!\r\n");
-        $i = 0;
+
         $units_to_print = array();
+        $i = 0;        
         while ($row = mysql_fetch_array($user_units))
         {
             $last_location = mysql_fetch_array($this->conn->get_unit_last_location($row['unit_id']));
@@ -36,8 +33,8 @@ class DataFetcher
             $i++;
         }
 
-        $this->conn->close();     
-        fclose($f);
+        $this->conn->close();   
+        
         return $units_to_print;
     }
 }
