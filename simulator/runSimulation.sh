7ce0e35a41fe84375420707e46004f6df6902403
Adam Narkusnki <airdong@gmail.com>
2013-05-17 14:54:25 +0300

    script is working

--- simulator/runSimulation.sh
+++ simulator/runSimulation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-for GPX in `ls gpx | grep *.gpx` ;  do
-	py simulator.py $GPX ${GPX:0:1} &
+for GPX in `ls gpx | grep gpx` ;  do
+	echo simulator.py $GPX ${GPX:0:1}
+	python simulator.py gpx/$GPX ${GPX:0:1} &
 done
