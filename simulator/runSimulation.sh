1fe7b9f7a62aafa7d1fa3bdc0bbe4e05458898f0
Adam Narkusnki <airdong@gmail.com>
2013-05-22 17:55:05 +0300

    as

--- simulator/runSimulation.sh
+++ simulator/runSimulation.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-DIR=$1
-#echo $DIR
+if [ -d $1 ] ; then
+	DIR=$1
+else
+	DIR=gpx
+fi
+
+#DIR=$1
+echo "DIR = $DIR"
 for GPX in `ls $1 | grep gpx` ;  do
 	echo "simulator.py $DIR/$GPX ${GPX:0:1}"
 	python simulator.py $DIR/$GPX ${GPX:0:1} &
