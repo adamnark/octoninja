3912846f4b9137bad4bfe3f2ea1a3b24bcbc6670
Adam Narkunski <airdong@gmail.com>
2013-06-04 18:44:46 +0300

    see email for details

--- website/gpsweb/management/commands/gps_server/emailer.py
+++ website/gpsweb/management/commands/gps_server/emailer.py
@@ -9,7 +9,7 @@ import os
 #             "unit_name":"car 1", 
 #             "speed":"0", 
 #             "location":"32.09150335%2034.788823",
-#             "format":"hello, %(name)s! %(unit_name)s was going......."
+#             "format":"<h1>hello, %(name)s!</h1> %(unit_name)s was going......."
 #             "timestamp":datetime.datetime.now(),
 #             "to":"amir_solav@gmail.com"}
 
@@ -17,9 +17,11 @@ def mail(args_dict): #args_dict has at least: timestamp, name, unit_name, locati
     gmail_user = "GPSMonitorEmailer@gmail.com"
     gmail_pwd = "adam123456"
     message = MIMEMultipart('alternative')
-    message['Subject'] = 'GPS Alert has gone off!'
+
+    message['Subject'] = args_dict["subject"] if "subject" in args_dict else 'GPS Alert has gone off!' 
     message['From'] = gmail_user
     message['To'] = args_dict['to']
+
     message.attach(MIMEText(args_dict["format"] % args_dict, 'html'))
     mailServer = smtplib.SMTP("smtp.gmail.com", 587)
     mailServer.ehlo()
@@ -27,9 +29,26 @@ def mail(args_dict): #args_dict has at least: timestamp, name, unit_name, locati
     mailServer.ehlo()
     mailServer.login(gmail_user, gmail_pwd)
     mailServer.sendmail(gmail_user, args_dict['to'], message.as_string())
-    # Should be mailServer.quit(), but that crashes...
     mailServer.close()
 
-#if __name__ == '__main__': main()
+
+def garbage():
+    html = """
+<html>
+  <body>
+     <p>Testin testin testin</p>
+     <p>Sent by LightFleet</p>
+  </body>
+</html>
+"""
+
+    args_dict = {
+            "to" : "airdong@gmail.com",
+            "format" : html ,
+            "subject" : "Daily LightFleet Report"
+        }
+
+    mail(args_dict)
+
 
 
