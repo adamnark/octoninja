a6c42cf40e2ef8903184f1c938e4ec310645b460
Amir Solav <amirsolav@gmail.com>
2013-07-24 22:12:01 +0300

    ssss

--- website/gpsweb/admin.py
+++ website/gpsweb/admin.py
@@ -1,6 +1,39 @@
 from django.contrib import admin
-#from django.contrib.auth.models import User
 from gpsweb.models import *
+from django.http import HttpResponse
+from django.core import serializers
+from utils.csvSerializer import Serializer as csvSerializer
+
+
+from pprint import pprint 
+
+def Export_as_json(modeladmin, request, queryset):
+    response = HttpResponse(content_type="application/json")
+    serializers.serialize("json", queryset, stream=response)
+    return response
+    
+def Export_as_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type="text/csv")
+    response['Content-Disposition'] = 'attachment; filename="%ss.csv"' % queryset[0].__class__.__name__
+    serializers.serialize("csv", queryset, stream=response)
+
+    return response
+
+admin.site.add_action(Export_as_csv)
+admin.site.add_action(Export_as_json)
+
+
+class LocationLogAdmin(admin.ModelAdmin):
+    list_filter = ('timestamp', 'car', 'driver', )
+
+class AlertLogAdmin(admin.ModelAdmin):
+    list_filter = ('notification_sent', 'marked_as_read', 'location_log__car', 'location_log__driver')
+
+class AlertAdmin(admin.ModelAdmin):
+    list_filter = ('car', 'type',)
+    
+class FuelConsumptionLogAdmin(admin.ModelAdmin):
+    list_filter = ('car', 'timestamp', 'station_id', ) 
 
 
 
@@ -9,11 +42,14 @@ admin.site.register(Person)
 admin.site.register(Car)
 admin.site.register(Driver)
 admin.site.register(TemporaryDriver)
-admin.site.register(Alert)
+admin.site.register(Alert, AlertAdmin)
+admin.site.register(LocationLog, LocationLogAdmin)
+admin.site.register(AlertLog, AlertLogAdmin)
+admin.site.register(PrimaryDriver)
+admin.site.register(FuelConsumptionLog, FuelConsumptionLogAdmin)
+
+
 admin.site.register(AlertArea)
 admin.site.register(AlertCircle)
 admin.site.register(AlertScheduleProfile)
-admin.site.register(LocationLog)
-admin.site.register(AlertLog)
-admin.site.register(PrimaryDriver)
-admin.site.register(FuelConsumptionLog)
+
--- website/mysite/settings.py
+++ website/mysite/settings.py
@@ -131,6 +131,10 @@ INSTALLED_APPS = (
     'gpsweb',
 )
 
+SERIALIZATION_MODULES = {
+    'csv' : 'gpsweb.utils.csvSerializer',
+}
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
